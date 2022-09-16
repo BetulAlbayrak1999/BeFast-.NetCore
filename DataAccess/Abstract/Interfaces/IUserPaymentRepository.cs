8287aad37c1fb56cd0c28939f7c40dbd2cfa6e6a
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-09-16 23:00:18 +0300

    add interfaces

---
+++ DataAccess/Abstract/Interfaces/IUserPaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Abstract.Common;
+using Entity.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract.Interfaces
+{
+    public interface IUserPaymentRepository : IBaseRepository<UserPayment>
+    {
+
+    }
+}
