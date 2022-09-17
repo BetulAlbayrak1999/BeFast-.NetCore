378f079dad10444d851412aab3a353974d7003f3
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-09-17 21:24:59 +0300

    add services but empty

---
+++ Business/Abstract/Interfaces/IPasswordResetService.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Abstract.Common;
+using Entity.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract.Interfaces
+{
+    public interface IPasswordResetService 
+    {
+
+    }
+}
